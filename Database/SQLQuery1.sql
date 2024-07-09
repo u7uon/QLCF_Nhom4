b752fc884b0b8f7f85f116efe0ffea6a1db80fe1
eJwtzMtuwjAQQNF/mbUXeRADXlZVaVlWRcDKGtsDuDGxNXZKI8S/I6Fsr3TuHdBayvkn9jSAgrWpXVNVi9asyTbtSVbOWJTOUme7VSslInbLqgMBieOfd8Qzvc8nXebV+RL1V5mO/W5ZPt55s3+LNW4P38cwXBbbRKGp97+fGwkCXkSXKREoMIRMDAKyja+A7uoHlUYTvNU9TYIJnYp8FkwpijETK7qiD+IWuT+FeAMB9J88U9ZYQA1jCI/HE68fUIg= <duongnvpd10563@gmail.com>
2024-07-09 11:19:22 +0700

    Bắt đầu giai đoạn 1

---
+++ Database/SQLQuery1.sql
@@ -0,0 +1,21 @@
+﻿create database QuanLyQuanCafe
+go
+use QuanLyQuanCafe
+
+
+CREATE TABLE NhanVien
+(
+    ID int IDENTITY(1,1) NOT NULL,
+    MaNV AS ('NV' + CAST(ID AS VARCHAR)) PERSISTED PRIMARY KEY,
+    Email varchar(50) NOT NULL UNIQUE,
+    TenNV nvarchar(100) NOT NULL,
+    VaiTro tinyint DEFAULT 0,
+    GioiTinh nvarchar(5) ,
+    SoDT varchar(13),
+    DiaChi nvarchar(100),
+    MatKhau varchar(300) NOT NULL
+);
+
+-- Chèn dữ liệu ví dụ
+INSERT INTO NhanVien (Email, TenNV, VaiTro, GioiTinh, SoDT, DiaChi, MatKhau)
+VALUES ('example@example.com', 'Nguyen Van A', 1, 'Nam', '0123456789', '123 Street', 'hashed_password');
